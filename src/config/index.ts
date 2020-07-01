469d7d7ec2ceee54fa9e54770e51275c1e24f276
humgchatbot <57216251+humgchatbot@users.noreply.github.com>
2020-07-01 22:23:48 +0700

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -81,7 +81,7 @@ export default {
   MAX_MESSAGE_LENGTH: parseEnvNumber('MAX_MESSAGE_LENGTH') || 2000,
 
   // App name (must be the same on Heroku)
-  APP_NAME: parseEnvString('APP_NAME') || 'ptnkchat',
+  APP_NAME: parseEnvString('APP_NAME') || 'humgchatbot',
 
   // Heroku API key
   HEROKU_API_KEY: parseEnvString('HEROKU_API_KEY') || '',
