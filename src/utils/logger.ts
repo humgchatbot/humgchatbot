b7682a6cd7b391abf1526ef02e4976a39653341c
humgchatbot <57216251+humgchatbot@users.noreply.github.com>
2020-07-02 20:17:49 +0700

    v5.0.0

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -44,8 +44,8 @@ const logPair = async (id1: string, id2: string): Promise<void> => {
   const info1 = await fb.getUserData(id1);
   const info2 = await fb.getUserData(id2);
   
-  data += "&entry."+config.POST_LOG_NAME1+"=" + data.name;
-  data += "&entry."+config.POST_LOG_NAME2+"=" + data.name;
+  data += "&entry."+config.POST_LOG_NAME1+"=" + data.userProfile;
+  data += "&entry."+config.POST_LOG_NAME2+"=" + data.userProfile;
     
   try {
     await phin({
