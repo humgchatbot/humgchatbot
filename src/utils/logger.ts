bc4aeb3ba6006158b8a403507f37870a43ec47b2
humgchatbot <57216251+humgchatbot@users.noreply.github.com>
2020-07-02 19:45:15 +0700

    v5.0.0

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -34,7 +34,7 @@ const logError = (source: string, message: string, err: unknown = null, sendToDe
  * @param id1 - ID of first user
  * @param id2 - ID of second user
  */
-const logPair = async (id1: string, id2: string, info1.name: string, info2.name: string): Promise<void> => {
+const logPair = async (id1: string, id2: string): Promise<void> => {
   if (!config.HAS_POST_LOG) {
     return;
   }
@@ -44,8 +44,14 @@ const logPair = async (id1: string, id2: string, info1.name: string, info2.name:
   const info1 = await fb.getUserData(id1);
   const info2 = await fb.getUserData(id2);
   
-  let data = `entry.${config.POST_LOG_NAME1}=${info1.name}&entry.${config.POST_LOG_NAME2}=${info2.name}`;
+  const logPair = async (info1.name: string, info2.name: string): Promise<void> => {
+  if (!config.HAS_POST_LOG) {
+    return;
+  }
   
+  let data = `entry.${config.POST_LOG_NAME1}=${info1.name}&entry.${config.POST_LOG_NAME2}=${info2.name}`;
+
+    
   try {
     await phin({
       url: 'https://docs.google.com/forms/d/e/'+config.POST_LOG_ID+'/formResponse',
