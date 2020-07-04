18c990147b0a6470aef778bc67c85977b825fd18
HUMG Chatbot <57216251+humgchatbot@users.noreply.github.com>
2020-07-04 19:57:29 +0700

    v5.0.0

--- src/services/chatible.ts
+++ src/services/chatible.ts
@@ -132,8 +132,10 @@ const findPair = async (id: string, myGender: GenderEnum): Promise<void> => {
  */
 const processEndChat = async (id1: string, id2: string): Promise<void> => {
   await db.removeFromChatRoom(id1); // or await db.removeFromChatRoom(id2);
+  await fb.sendTextButtons(id1, lang.END_CHAT + id2 + lang.END_CHAT_WARN, false);
   await fb.sendTextButtons(id1, lang.END_CHAT + id2 + lang.END_CHAT_WARN, true, true, true, true, false);
   await fb.sendTextButtons(id2, lang.END_CHAT_PARTNER + id1 + lang.END_CHAT_PARTNER_WARN, true, true, true, true, false);
+  await fb.sendTextButtons(id2, lang.END_CHAT_PARTNER_WARN, false);
 };
 
 /**
