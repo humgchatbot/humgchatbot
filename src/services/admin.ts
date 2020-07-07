df00023cd49e3cc00b3cd504ffff90f53b62f0cf
HUMG Chatbot <57216251+humgchatbot@users.noreply.github.com>
2020-07-07 08:19:07 +0700

    v5.0.0

--- src/services/admin.ts
+++ src/services/admin.ts
@@ -134,14 +134,14 @@ const forceMatch = async (
 
   const partner1: string | null = await db.findPartnerChatRoom(id1);
   const partner2: string | null = await db.findPartnerChatRoom(id2);
-  if (partner1 === null && partner2 === null) {
-      await db.writeToChatRoom(id1, id2, gender1, gender2);
-      await fb.sendTextMessage('', id1, lang.CONNECTED + id2 + lang.CONNECTED_WARN, false);
-      await fb.sendTextMessage('',id1,lang.CONNECT_NOTE, false);
-      await fb.sendTextMessage('', id2, lang.CONNECTED + id1 + lang.CONNECTED_WARN, false);
-      await fb.sendTextMessage('',id2, lang.CONNECT_NOTE, false);
-      await logger.logPair(id1, id2);
-    }
+if (partner1 === null && partner2 === null) {
+    await db.writeToChatRoom(id1, id2, gender1, gender2);
+    await fb.sendTextMessage('', id1, lang.CONNECTED + id2 + lang.CONNECTED_WARN, false);
+    await fb.sendTextMessage('',id1,lang.CONNECT_NOTE, false);
+    await fb.sendTextMessage('', id2, lang.CONNECTED + id1 + lang.CONNECTED_WARN, false);
+    await fb.sendTextMessage('',id2, lang.CONNECT_NOTE, false);
+    await logger.logPair(id1, id2);
+  }
   return { success: true, error: false };
 };
 
