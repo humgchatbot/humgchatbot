2df12455a850badbd220b83fa732307ec72c5165
HUMG Chatbot <57216251+humgchatbot@users.noreply.github.com>
2020-08-23 15:49:21 +0700

    v5.0.0

--- src/config/index.ts
+++ src/config/index.ts
@@ -101,10 +101,10 @@ export default {
   POST_LOG_NAME2: parseEnvString('POST_LOG_NAME2') || '',
 
   // Link to Google Form for reporting
-  REPORT_LINK: parseEnvString('REPORT_LINK') || 'docs.google.com/forms/d/e/1FAIpQLScRqbztpcLLkUexkovOaBkuPLnS7vTaq_NCbtlS3etwQy5EtQ/viewform?usp=sf_link ',
-  TSAD_LINK: parseEnvString('TSAD_LINK') || 'https://m.me/adschatbot',
-  CFS_LINK: parseEnvString('CFS_LINK') || 'docs.google.com/forms/d/e/1FAIpQLScTbMFkbadN3t5y6OHh1IIZ4luqrswqqZOJ3A2YR0JyWnSRWg/viewform?usp=sf_link ',
-  GROPUP_LINK: parseEnvString('GROPUP_LINK') || 'facebook.com/groups/humgbotchat/',
+  REPORT_LINK: parseEnvString('REPORT_LINK') || '',
+  TSAD_LINK: parseEnvString('TSAD_LINK') || '',
+  CFS_LINK: parseEnvString('CFS_LINK') || '', //docs.google.com/forms/d/e/1FAIpQLScTbMFkbadN3t5y6OHh1IIZ4luqrswqqZOJ3A2YR0JyWnSRWg/viewform?usp=sf_link 
+  GROPUP_LINK: parseEnvString('GROPUP_LINK') || '',
   
   // Maximum number of people in wait room
   MAX_PEOPLE_IN_WAITROOM: parseEnvNumber('MAX_PEOPLE_IN_WAITROOM') || 10,
